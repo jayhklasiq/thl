afcbad7de2fde9a157e5807131a72c803e037747
jayhklasiq <klasik@byui.edu>
2025-05-20 08:37:10 +0000

    Update SMTP host in send-email.php from Gmail to Zoho

--- send-email.php
+++ send-email.php
@@ -19,7 +19,7 @@ try {
   $mail->isSMTP();
   $mail->SMTPAuth = true;
   $mail->SMTPSecure = 'ssl';
-  $mail->Host = 'smtp.gmail.com';
+  $mail->Host = 'smtp.zoho.com';
   $mail->Port = '465';
   $mail->isHTML(false);
 
